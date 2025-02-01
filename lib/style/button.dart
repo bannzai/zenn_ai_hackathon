28a13b58e02b275f11399fb70de5feda8d0449e6
bannzai <kingkong999yhirose@gmail.com>
2025-02-01 21:20:33 +0900

    Add alert button style

--- lib/style/button.dart
+++ lib/style/button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todomaker/style/color.dart';
 
 ButtonStyle get filledWidthButtonStyle => ButtonStyle(
       minimumSize: WidgetStateProperty.all(const Size(double.infinity, 48.0)),
@@ -11,6 +12,22 @@ ButtonStyle capsuleTextButtonStyle(BuildContext context) {
     backgroundColor: Colors.white,
     padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
     shape: const StadiumBorder(),
+    elevation: 1,
+  );
+}
+
+ButtonStyle alertButtonStyle(BuildContext context) {
+  final foregroundColor = Theme.of(context).colorScheme.primary;
+  return TextButton.styleFrom(
+    foregroundColor: foregroundColor,
+    disabledForegroundColor: TextColor.disabled(TextColor.gray),
+    backgroundColor: Colors.white,
+    padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+    shape: const StadiumBorder(),
+    textStyle: const TextStyle(
+      fontWeight: FontWeight.w600,
+      fontSize: 14,
+    ),
     elevation: 1,
   );
 }
