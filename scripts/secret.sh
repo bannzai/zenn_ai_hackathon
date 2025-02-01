eadd2ec6a76b8b3ead8be4637d1bbeb6e209f272
bannzai <kingkong999yhirose@gmail.com>
2025-02-01 16:24:30 +0900

    Add secret.sh script

---
+++ scripts/secret.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+cat lib/secret/secret.dart.sample | sed \
+  -e "s|\[REVENUE_CAT_PUBLIC_API_KEY\]|$REVENUE_CAT_PUBLIC_API_KEY|g" \
+> lib/secret/secret.dart
+
+
+
