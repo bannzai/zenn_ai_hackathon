aa643d1ff7cdefcd09975ee31005ba8f28309938
bannzai <kingkong999yhirose@gmail.com>
2025-02-01 21:27:32 +0900

    Fix resolver

--- lib/features/root/page.dart
+++ lib/features/root/page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:todomaker/features/home/page.dart';
-import 'package:todomaker/features/root/resolver/app_resolvers.dart';
 import 'package:todomaker/features/root/resolver/app_user_create.dart';
+import 'package:todomaker/features/root/resolver/auth.dart';
+import 'package:todomaker/features/root/resolver/database.dart';
 import 'package:todomaker/features/root/resolver/force_update.dart';
 import 'package:todomaker/features/root/resolver/purchase_setup.dart';
 
@@ -11,15 +12,17 @@ class RootPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return AppResolvers(builder: (context, user) {
-      return AppUserCreateResolver(builder: (context) {
-        return ForceUpdateResolver(builder: (context) {
-          return PurchaseSetupResolver(
-            userID: user.uid,
-            builder: (context) {
-              return const HomePage();
-            },
-          );
+    return AuthResolver(builder: (context, user) {
+      return UserDatabaseResolver(builder: (context) {
+        return AppUserCreateResolver(builder: (context) {
+          return ForceUpdateResolver(builder: (context) {
+            return PurchaseSetupResolver(
+              userID: user.uid,
+              builder: (context) {
+                return const HomePage();
+              },
+            );
+          });
         });
       });
     });
