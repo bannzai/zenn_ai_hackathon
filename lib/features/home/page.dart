2db6e485b332c8e9a694dbf959e9ab15275a26ae
bannzai <kingkong999yhirose@gmail.com>
2025-02-02 13:43:49 +0900

    Add tasks page

--- lib/features/home/page.dart
+++ lib/features/home/page.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+enum HomePageTabType { tasks, profile }
+
 class HomePage extends HookConsumerWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Text('HomePage');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomePage'),
+      ),
+      body: ListView(
+        children: [],
+      ),
+    );
   }
 }
