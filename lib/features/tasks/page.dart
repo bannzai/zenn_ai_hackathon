c21c372e63a4532340f4348138f0de36b1d45105
bannzai <kingkong999yhirose@gmail.com>
2025-02-03 18:18:57 +0900

    Fix

--- lib/features/tasks/page.dart
+++ lib/features/tasks/page.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:todomaker/components/loading/indicator.dart';
 import 'package:todomaker/components/retry/page.dart';
+import 'package:todomaker/components/todos/list.dart';
 import 'package:todomaker/entity/task.dart';
-import 'package:todomaker/entity/todo.dart';
 import 'package:todomaker/provider/task.dart';
-import 'package:todomaker/provider/todo.dart';
 import 'package:todomaker/style/color.dart';
 import 'package:todomaker/utils/network/cloud_run.dart';
 
@@ -112,62 +110,6 @@ class TasksPageSection extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-}
-
-class TasksTodoList extends HookConsumerWidget {
-  final String taskID;
-  const TasksTodoList({super.key, required this.taskID});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final todos = ref.watch(todosProvider(taskID: taskID));
-
-    return Retry(
-      retry: () => ref.invalidate(todosProvider(taskID: taskID)),
-      child: todos.when(
-        data: (todos) => Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            for (final todo in todos) ...[
-              TasksPageTodoRow(todo: todo),
-            ],
-          ],
-        ),
-        error: (error, stackTrace) => RetryPage(exception: error, stackTrace: stackTrace),
-        loading: () => const IndicatorPage(),
-      ),
-    );
-  }
-}
-
-class TasksPageTodoRow extends HookConsumerWidget {
-  final Todo todo;
-  const TasksPageTodoRow({super.key, required this.todo});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final completed = useState(todo.completed);
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        Checkbox(
-          value: completed.value,
-          onChanged: (value) {
-            completed.value = value ?? false;
-          },
-        ),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(todo.content, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-              Text(todo.supplement, style: const TextStyle(fontSize: 14), maxLines: 2),
-            ],
-          ),
-        ),
-      ],
     );
   }
 }
