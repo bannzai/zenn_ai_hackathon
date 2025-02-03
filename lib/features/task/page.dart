0e71521c81fe17ed809606a0641d2299bf077e29
bannzai <kingkong999yhirose@gmail.com>
2025-02-03 18:19:06 +0900

    Add TaskPage

---
+++ lib/features/task/page.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:todomaker/components/loading/indicator.dart';
+import 'package:todomaker/components/retry/page.dart';
+import 'package:todomaker/components/todos/list.dart';
+import 'package:todomaker/entity/task.dart';
+import 'package:todomaker/provider/task.dart';
+import 'package:todomaker/style/color.dart';
+
+class TaskPage extends HookConsumerWidget {
+  final String taskID;
+  const TaskPage({super.key, required this.taskID});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final task = ref.watch(taskProvider(taskID: taskID));
+
+    return Retry(
+      retry: () => ref.invalidate(taskProvider(taskID: taskID)),
+      child: task.when(
+        data: (task) => TaskPageBody(task: task),
+        error: (error, stackTrace) => RetryPage(exception: error, stackTrace: stackTrace),
+        loading: () => const IndicatorPage(),
+      ),
+    );
+  }
+}
+
+class TaskPageBody extends StatelessWidget {
+  final Task task;
+  const TaskPageBody({super.key, required this.task});
+
+  @override
+  Widget build(BuildContext context) {
+    final primaryColor = Theme.of(context).colorScheme.primary;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(8),
+          border: Border.all(color: AppColors.border),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: const Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              task.question,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: primaryColor,
+              ),
+            ),
+            const SizedBox(height: 4),
+            Text(
+              task.aiTextResponse,
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+              maxLines: 5,
+            ),
+            const SizedBox(height: 4),
+            TasksTodoList(taskID: task.id),
+          ],
+        ),
+      ),
+    );
+  }
+}
