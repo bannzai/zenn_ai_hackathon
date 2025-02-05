a972e5887c51f49f4330acf8aa540a1d8d537c4c
bannzai <kingkong999yhirose@gmail.com>
2025-02-05 11:49:04 +0900

    Remove unnecessary import

--- lib/components/todo/list.dart
+++ lib/components/todo/list.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:todomaker/components/loading/indicator.dart';
 import 'package:todomaker/components/retry/button.dart';
