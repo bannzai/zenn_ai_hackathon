6675735326b21646ec06c29bc3b0fc48065257c9
bannzai <kingkong999yhirose@gmail.com>
2025-02-07 16:28:30 +0900

    add minheight

--- lib/features/tasks/components/section.dart
+++ lib/features/tasks/components/section.dart
@@ -20,75 +20,78 @@ class TasksPageSection extends StatelessWidget {
 
     return Stack(
       children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Container(
-            padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(8),
-              border: Border.all(color: AppColors.border),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.1),
-                  spreadRadius: 2,
-                  blurRadius: 5,
-                  offset: const Offset(0, 3),
-                ),
-              ],
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    Text(
-                      task.question,
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: primaryColor,
+        Container(
+          constraints: const BoxConstraints(minHeight: 120),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8),
+                border: Border.all(color: AppColors.border),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                    offset: const Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        task.question,
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: primaryColor,
+                        ),
                       ),
-                    ),
-                    const Spacer(),
-                    GestureDetector(
-                      onTap: () {
-                        if (task is TaskPrepared) {
-                          Navigator.push(context, MaterialPageRoute(builder: (context) => TaskPage(task: task)));
-                        }
-                      },
-                      child: const Row(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          Text('詳細を見る', style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold, color: TextColor.link)),
-                          SizedBox(width: 2),
-                          Icon(Icons.chevron_right, color: TextColor.link, size: 16),
-                        ],
+                      const Spacer(),
+                      GestureDetector(
+                        onTap: () {
+                          if (task is TaskPrepared) {
+                            Navigator.push(context, MaterialPageRoute(builder: (context) => TaskPage(task: task)));
+                          }
+                        },
+                        child: const Row(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Text('詳細を見る', style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold, color: TextColor.link)),
+                            SizedBox(width: 2),
+                            Icon(Icons.chevron_right, color: TextColor.link, size: 16),
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 10),
-                if (definitionAITextResponse != null) ...[
-                  Text(
-                    definitionAITextResponse,
-                    style: const TextStyle(fontSize: 14),
-                    maxLines: 3,
-                    overflow: TextOverflow.ellipsis,
+                    ],
                   ),
                   const SizedBox(height: 10),
+                  if (definitionAITextResponse != null) ...[
+                    Text(
+                      definitionAITextResponse,
+                      style: const TextStyle(fontSize: 14),
+                      maxLines: 3,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    const SizedBox(height: 10),
+                  ],
+                  if (task is TaskPrepared) ...[
+                    const Divider(height: 1, color: Colors.black),
+                    const SizedBox(height: 20),
+                    TasksTodoList(task: task, limit: 3),
+                    const Divider(height: 1, color: Colors.black),
+                    const SizedBox(height: 16),
+                  ],
+                  if (todosGroundings != null) ...[
+                    GroundingDataList(groundings: todosGroundings),
+                  ],
                 ],
-                if (task is TaskPrepared) ...[
-                  const Divider(height: 1, color: Colors.black),
-                  const SizedBox(height: 20),
-                  TasksTodoList(task: task, limit: 3),
-                  const Divider(height: 1, color: Colors.black),
-                  const SizedBox(height: 16),
-                ],
-                if (todosGroundings != null) ...[
-                  GroundingDataList(groundings: todosGroundings),
-                ],
-              ],
+              ),
             ),
           ),
         ),
