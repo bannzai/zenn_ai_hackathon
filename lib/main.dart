4466df6b19e505489968f430dee3a465591b8d04
bannzai <kingkong999yhirose@gmail.com>
2025-02-08 20:53:20 +0900

    Remove unnecessary code

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:todomaker/app.dart';
-import 'package:geocoding/geocoding.dart';
 import 'package:todomaker/utils/log/debug_print.dart';
 
 void main() async {
@@ -19,8 +18,6 @@ void main() async {
 
     // MEMO: FirebaseCrashlytics#recordFlutterError called dumpErrorToConsole in function.
     FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
-
-    setLocaleIdentifier('ja');
 
     runApp(const ProviderScope(child: App()));
   }, (error, stack) => FirebaseCrashlytics.instance.recordError(error, stack));
