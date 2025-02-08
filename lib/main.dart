647a7e13ac1b68bcd81f418567dcaab175eafc07
bannzai <kingkong999yhirose@gmail.com>
2025-02-08 22:23:17 +0900

    Fix entrypoint

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,10 @@ import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todomaker/app.dart';
+import 'package:todomaker/provider/shared_preferences.dart';
+import 'package:todomaker/utils/config/remote_config.dart';
 import 'package:todomaker/utils/log/debug_print.dart';
 
 void main() async {
@@ -16,9 +19,21 @@ void main() async {
     WidgetsFlutterBinding.ensureInitialized();
     await Firebase.initializeApp();
 
+    final (sharedPreferences, _) = await (
+      SharedPreferences.getInstance(),
+      setupRemoteConfig(),
+    ).wait;
+
     // MEMO: FirebaseCrashlytics#recordFlutterError called dumpErrorToConsole in function.
     FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
 
-    runApp(const ProviderScope(child: App()));
+    runApp(
+      ProviderScope(
+        overrides: [
+          sharedPreferencesProvider.overrideWith((ref) => sharedPreferences),
+        ],
+        child: const App(),
+      ),
+    );
   }, (error, stack) => FirebaseCrashlytics.instance.recordError(error, stack));
 }
