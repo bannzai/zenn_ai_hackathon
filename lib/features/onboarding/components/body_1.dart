0a8d80a3a85c872b3bc45ae0e4e2ac92c8c7920b
bannzai <kingkong999yhirose@gmail.com>
2025-02-09 05:14:56 +0900

    Extract page

---
+++ lib/features/onboarding/components/body_1.dart
@@ -0,0 +1,98 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/material.dart';
+
+class OnboardingBody1 extends StatelessWidget {
+  const OnboardingBody1({
+    super.key,
+    required this.index,
+    required this.animationController,
+  });
+
+  final ValueNotifier<int> index;
+  final AnimationController animationController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.bottomCenter,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Spacer(),
+            OnboardingBot1(
+              messages: const [
+                'こんにちは！',
+                'TODOMakerへようこそ',
+              ],
+              onFinished: () => index.value++,
+            ),
+            const Spacer(),
+          ],
+        ),
+        if (index.value <= 1) ...[
+          Positioned(
+            bottom: 20,
+            child: SizedBox(
+              height: 100,
+              child: FadeTransition(
+                opacity: Tween<double>(begin: 0, end: 1).animate(
+                  CurvedAnimation(
+                    parent: animationController,
+                    curve: Curves.easeInOut,
+                  ),
+                ),
+                child: const Text(
+                  'タップして次へ',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black,
+                    shadows: [Shadow(color: Colors.blueGrey, blurRadius: 2)],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ],
+    );
+  }
+}
+
+class OnboardingBot1 extends StatelessWidget {
+  final List<String> messages;
+  final VoidCallback onFinished;
+
+  const OnboardingBot1({super.key, required this.messages, required this.onFinished});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 100,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Text('🤖'),
+          const SizedBox(width: 2),
+          Container(
+            constraints: const BoxConstraints(maxWidth: 300),
+            child: AnimatedTextKit(
+                isRepeatingAnimation: false,
+                animatedTexts: [
+                  for (var message in messages) TyperAnimatedText(message, speed: const Duration(milliseconds: 90)),
+                ],
+                onNext: (index, isLast) {
+                  debugPrint('index: $index, isLast: $isLast');
+                  if (isLast) {
+                    onFinished();
+                  }
+                }),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/features/onboarding/page.dart
+++ lib/features/onboarding/page.dart
@@ -1,6 +1,7 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:todomaker/features/onboarding/components/body_1.dart';
 
 class OnboardingPage extends HookWidget {
   final ValueNotifier<bool> isOnboardingResolved;
@@ -27,88 +28,8 @@ class OnboardingPage extends HookWidget {
           ],
         ),
         child: SafeArea(
-          child: Stack(
-            alignment: Alignment.bottomCenter,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const Spacer(),
-                  OnboardingBot1(
-                    messages: const [
-                      'こんにちは！',
-                      'TODOMakerへようこそ',
-                    ],
-                    onFinished: () => index.value++,
-                  ),
-                  const Spacer(),
-                ],
-              ),
-              if (index.value <= 1) ...[
-                Positioned(
-                  bottom: 20,
-                  child: SizedBox(
-                    height: 100,
-                    child: FadeTransition(
-                      opacity: Tween<double>(begin: 0, end: 1).animate(
-                        CurvedAnimation(
-                          parent: animationController,
-                          curve: Curves.easeInOut,
-                        ),
-                      ),
-                      child: const Text(
-                        'タップして次へ',
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.black,
-                          shadows: [Shadow(color: Colors.blueGrey, blurRadius: 2)],
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ],
-          ),
+          child: OnboardingBody1(index: index, animationController: animationController),
         ),
-      ),
-    );
-  }
-}
-
-class OnboardingBot1 extends StatelessWidget {
-  final List<String> messages;
-  final VoidCallback onFinished;
-
-  const OnboardingBot1({super.key, required this.messages, required this.onFinished});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      height: 100,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          const Text('🤖'),
-          const SizedBox(width: 2),
-          Container(
-            constraints: const BoxConstraints(maxWidth: 300),
-            child: AnimatedTextKit(
-                isRepeatingAnimation: false,
-                animatedTexts: [
-                  for (var message in messages) TyperAnimatedText(message, speed: const Duration(milliseconds: 90)),
-                ],
-                onNext: (index, isLast) {
-                  debugPrint('index: $index, isLast: $isLast');
-                  if (isLast) {
-                    onFinished();
-                  }
-                }),
-          ),
-        ],
       ),
     );
   }
