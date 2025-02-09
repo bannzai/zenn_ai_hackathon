b8158af8b3332d0da09cd0e5200c06f285160ec9
bannzai <kingkong999yhirose@gmail.com>
2025-02-09 17:41:22 +0900

    WIP add location component

---
+++ lib/features/task/components/location.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:todomaker/entity/location.dart';
+import 'package:todomaker/entity/task.dart';
+import 'package:todomaker/entity/todo.dart';
+
+class TaskLocation extends StatelessWidget {
+  final TaskPrepared task;
+  final List<Todo> todos;
+  const TaskLocation({super.key, required this.task, required this.todos});
+
+  @override
+  Widget build(BuildContext context) {
+    final locations = task.locations;
+    final locationsAITextResponse = task.locationsAITextResponse ?? '';
+    final locationsGroundings = task.locationsGroundings ?? [];
+    if (locations == null) {
+      return TaskLocationEmpty(task: task);
+    }
+
+    return Column(
+      children: [
+        Text(task.topic),
+      ],
+    );
+  }
+}
+
+class TaskLocationEmpty extends StatelessWidget {
+  final TaskPrepared task;
+  const TaskLocationEmpty({super.key, required this.task});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox.shrink();
+  }
+}
+
+class TaskLocationItem extends StatelessWidget {
+  final TaskPrepared task;
+  final Location location;
+  const TaskLocationItem({
+    super.key,
+    required this.task,
+    required this.location,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox.shrink();
+  }
+}
+
+class TaskLocationTODOLocationList extends StatelessWidget {
+  final TaskPrepared task;
+  final List<Todo> todos;
+  const TaskLocationTODOLocationList({super.key, required this.task, required this.todos});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox.shrink();
+  }
+}
+
+class TaskLocationTODOLocationItem extends StatelessWidget {
+  final TaskPrepared task;
+  final Todo todo;
+  final Location todoLocation;
+  const TaskLocationTODOLocationItem({super.key, required this.task, required this.todo, required this.todoLocation});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox.shrink();
+  }
+}
