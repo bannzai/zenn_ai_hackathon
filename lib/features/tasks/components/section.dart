2b3cf2776ed3e31c976cfe36a72a6306a24453af
bannzai <kingkong999yhirose@gmail.com>
2025-02-10 08:56:20 +0900

    Fix

--- lib/features/tasks/components/section.dart
+++ lib/features/tasks/components/section.dart
@@ -99,7 +99,7 @@ class TasksPageSection extends HookConsumerWidget {
         ),
         if (task is TaskPreparing) ...[
           BotLoading(
-            messages: const ['準備中...', '1分ほど待ってね😘', '丁寧にWebから情報を収集中🦾'],
+            messages: const ['準備中...', '1分ほど待ってね😘', 'Webから情報を収集中🦾'],
             onStop: () {
               // TODO: Retry or エラーハンドリングの仕組みをちゃんと作る。ハッカソンだからとりあえず動くコードにしている
               ref.read(userDatabaseProvider).taskReference(taskID: task.id).delete();
